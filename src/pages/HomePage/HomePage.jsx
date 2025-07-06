5d69e9c22ef90f9f28cb918679d56cc7372e2aca
Shabb007 <sebkay2002@hotmail.com>
2025-07-06 18:16:23 +0300

    sunday

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,18 +6,20 @@ import "modern-normalize";
 const HomePage = () => {
   const navigate = useNavigate();
   return (
-    <div className={styles.hero}>
-      <h1 className={styles.heroH1}>Campers of your dreams</h1>
-      <p className={styles.heroText}>
-        You can find everything you want in our catalog.
-      </p>
-      <button
-        className={styles.heroButton}
-        onClick={() => navigate("/catalog")}
-      >
-        View now
-      </button>
-    </div>
+    <section className={styles.hero}>
+      <div className={styles.hero_wrapper}>
+        <h1 className={styles.heroH1}>Campers of your dreams</h1>
+        <p className={styles.heroText}>
+          You can find everything you want in our catalog.
+        </p>
+        <button
+          className={styles.heroButton}
+          onClick={() => navigate("/catalog")}
+        >
+          View now
+        </button>
+      </div>
+    </section>
   );
 };
 
