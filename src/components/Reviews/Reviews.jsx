78470e15ba512ef6c91c7fa68b9464daf7461d96
Shabb007 <sebkay2002@hotmail.com>
2025-07-09 21:56:56 +0300

    correction

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 import { selectOne } from "../../redux/trucksSlice";
 import css from "./Reviews.module.css";
 import Review from "../Review/Review";
-import { useId } from "react";
 
 const Reviews = () => {
   const truck = useSelector(selectOne);
